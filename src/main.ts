7f3698ce46a13e520331d4d524766402f331b844
Nathanaël <nathanael.elisabeth@ecole404.fr>
2023-02-02 13:15:31 +0100

    [task] fist commit of new version

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { RedisService } from "./redis/redisService.class";
+import { RedisPublication } from "./redis/redisPublication.class";
+import { RedisPublicationDto } from "./redis/redisPublicationDto";
+
+export { RedisService, RedisPublicationDto, RedisPublication };
---
+++ src/redis/redisPublicationDto.ts
@@ -0,0 +1,10 @@
+/**
+ * CREATED AT: 2023-02-01
+ * UPDATED AT: 2023-02-01 
+ * 
+ * AUTHOR: ELISABETH Nathanaël
+ */
+export interface RedisPublicationDto {
+    publishedData: any;
+    answerChannel?: string;
+}
